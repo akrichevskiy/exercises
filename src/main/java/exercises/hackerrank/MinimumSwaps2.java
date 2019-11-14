2e2f5120943146efd916616a9807e213ec9416e6
akrichevskiy <akrichevskiy@bol.com>
2019-11-14 20:16:35 +0100

    MinimumSwaps

--- src/main/java/exercises/hackerrank/MinimumSwaps2.java
+++ src/main/java/exercises/hackerrank/MinimumSwaps2.java
@@ -1,6 +1,56 @@
 package exercises.hackerrank;
 
+import exercises.ctci.ch1.PalindromePermutations;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MinimumSwaps2 {
+static class Pair {
+    int data, index;
+
+    Pair(int d, int i) {
+        this.data = d;
+        this.index = i;
+    }
+}
+
+static int minimumSwaps2(int[] arr) {
+    int i,j;
+    int answer = 0;
+    ArrayList<Pair> pairArray = new ArrayList();
+
+    for(i = 0; i < arr.length; i++) {
+        pairArray.add(new Pair(arr[i], i));
+    }
+
+    pairArray.sort((o1, o2) -> {
+        if(o1.data < o2.data) {
+            return  -1;
+        } else if (o1.data == o2.data) {
+            return 0;
+        } else {
+            return 1;
+        }
+    });
+
+    Boolean[] visited = new Boolean[arr.length];
+    Arrays.fill(visited, false);
+    for(i = 0; i < arr.length; i++) {
+        if(visited[i] || pairArray.get(i).index == i) {
+            continue;
+        }
+        j = i;
+        int cycles = 0;
+        while(!visited[j]) {
+            visited[j] = true;
+
+            j = pairArray.get(j).index;
+            cycles++;
+        }
+        answer += (cycles - 1);
+    }
+    return answer;
+}
 
 static int minimumSwaps(int[] arr) {
     int swaps = 0;
--- src/test/java/exercises/hackerrank/MinimumSwaps2Test.java
+++ src/test/java/exercises/hackerrank/MinimumSwaps2Test.java
@@ -5,10 +5,18 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MinimumSwaps2Test {
-    @Test
-    public void test(){
-        int [] arr = {2,3,4,1,5};
-        assertEquals(3, MinimumSwaps2.minimumSwaps(arr));
-    }
+  @Test
+  public void test() {
+    int[] arr = {2, 3, 4, 1, 5};
+    assertEquals(3, MinimumSwaps2.minimumSwaps(arr));
+  }
 
+  @Test
+  public void test2() {
+    int[] arr = {2, 4, 3, 1};
+    assertEquals(2, MinimumSwaps2.minimumSwaps2(arr));
+
+    int[] arr2 = {4,3,1,2};
+    assertEquals(3, MinimumSwaps2.minimumSwaps2(arr2));
+  }
 }
