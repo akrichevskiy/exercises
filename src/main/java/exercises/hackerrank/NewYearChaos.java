08b0e46d05a608c1fe2d4ef30ad8a84a8ee28777
akrichevskiy <akrichevskiy@bol.com>
2019-11-18 21:27:17 +0100

    New Year chaos

--- src/main/java/exercises/hackerrank/NewYearChaos.java
+++ src/main/java/exercises/hackerrank/NewYearChaos.java
@@ -2,30 +2,56 @@ package exercises.hackerrank;
 
 public class NewYearChaos {
 
-    static void minimumBribes(int[] q) {
-        int cnt = 0;
-        for (int i =0; i< q.length;i++){
-           int size = 0;
-           for(int j = i; j < q.length  -1; j++) {
-               if(q[j] > q[j+1]) {
-                   size++;
-                   int t = q[j];
-                   q[j] = q[j+1];
-                   q[j+1] = t;
-               }
-           }
-           size--;
-            if(size  > 2) {
-                cnt = -1;
-                break;
-            } else if (size != 0){
-                cnt++;
-            }
+  static void minimumBribes2(int[] q) {
+    int cnt = 0;
+    int i, j;
+    for (i = 0; i < q.length; i++) {
+      int br = 0;
+      j = i + 1;
+      while (j < q.length) {
+        if (q[i] > q[j]) {
+          br++;
         }
-        if (cnt != -1) {
-            System.out.println(cnt);
-        } else {
-            System.out.println("Too chaotic");
-        }
+        j++;
+      }
+      if (br > 2) {
+        cnt = -1;
+        break;
+      }
+      cnt += br;
     }
+    if (cnt != -1) {
+      System.out.println(cnt);
+    } else {
+      System.out.println("Too chaotic");
+    }
+  }
+
+  static void minimumBribes(int[] q) {
+    int cnt = 0;
+    int i;
+    for (i = q.length - 1; i >= 1; i--) {
+      if (q[i] == i + 1) {
+        continue;
+      } else if (q[i - 1] == i + 1) {
+        cnt++;
+        int t = q[i - 1];
+        q[i - 1] = q[i];
+        q[i] = t;
+      } else if (i >= 2 && q[i - 2] == i + 1) {
+          cnt += 2;
+          q[i - 2] = q[i - 1];
+          q[i - 1] = q[i];
+          q[i] = i + 1;
+      } else {
+        cnt = -1;
+        break;
+      }
+    }
+    if (cnt != -1) {
+      System.out.println(cnt);
+    } else {
+      System.out.println("Too chaotic");
+    }
+  }
 }
--- src/test/java/exercises/hackerrank/NewYearChaosTest.java
+++ src/test/java/exercises/hackerrank/NewYearChaosTest.java
@@ -1,16 +1,20 @@
 package exercises.hackerrank;
 
-
 import org.junit.Test;
 
 public class NewYearChaosTest {
 
-    @Test
-    public void test() {
-        int [] a= {2, 1, 5, 3, 4};
-//        int [] a= {2, 5, 1, 3, 4};
-        NewYearChaos.minimumBribes(a);
+  @Test
+  public void test() {
+    int[] a = {2, 1, 5, 3, 4};
+    //        int [] a= {2, 5, 1, 3, 4};
+    NewYearChaos.minimumBribes2(a);
+  }
 
-    }
-
+  @Test
+  public void test2() {
+//    int[] a = {2, 1, 5, 3, 4};
+            int [] a= {2, 5, 1, 3, 4};
+    NewYearChaos.minimumBribes(a);
+  }
 }
